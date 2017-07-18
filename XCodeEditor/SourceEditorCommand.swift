d0090f4f0dc0752ef8b9a897c699122ca24ddcba
Thomas Kobert <surkin@teppichroller.local>
2017-07-18 05:30:42 +0200

    Initial Commit

---
+++ XCodeEditor/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  XCodeEditor
+//
+//  Created by Thomas Kobert on 18.07.17.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
